39a8159376b91e8a8f1b64f208829ddbe57d732c
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-06-25 17:56:30 +0300

    Broken version

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
 
+/* app.get('/health', (req, res) => {
+  res.send('ok')
+}) */
+
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
